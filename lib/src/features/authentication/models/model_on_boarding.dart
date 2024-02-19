c614058184f4e4d1586695c9573f623ff4d11e24
zobaer53 <hossain.zobaer11858@gmail.com>
2024-02-19 17:50:09 +0600

    refactor -- reusable widget created

--- lib/src/features/authentication/models/model_on_boarding.dart
+++ lib/src/features/authentication/models/model_on_boarding.dart
@@ -1,10 +1,15 @@
 
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
 class OnBoardingModel{
   final String image;
   final String title;
   final String subTitle;
   final String counterText;
-  final String bgColor;
+  final Color bgColor;
+  final double height;
 
   OnBoardingModel({
     required this.image,
@@ -12,5 +17,6 @@ class OnBoardingModel{
     required this.subTitle,
     required this.counterText,
     required this.bgColor,
+    required this.height,
 });
 }
--- lib/src/features/authentication/screens/on_boarding/on_boarding_screen.dart
+++ lib/src/features/authentication/screens/on_boarding/on_boarding_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_login_app/src/constants/sizes.dart';
 import 'package:flutter_login_app/src/constants/text_strings.dart';
+import 'package:flutter_login_app/src/features/authentication/models/model_on_boarding.dart';
 import 'package:flutter_login_app/src/utils/theme/colors.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
 
@@ -18,55 +19,17 @@ class OnBoardingScreen extends StatelessWidget {
         children: [
           LiquidSwipe(
             pages: [
-              Container(
-                padding: const EdgeInsets.all(zDefaultSizes),
-                color: zOnBoardingPage1Color,
-                child: Column(
-                  /*
-                * main axis alignment in column means vertical alignment
-                * by default main axis size is max
-                * and cross axis alignment set to center
-                * which is horizontal alignment
-                * */
-
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Image.asset(
-                      Assets.onboardingOnboarding1,
-                      height: size.height * .5,
-                    ), //50% of devices height
-
-                    /*
-                    * column bellow will not be effected
-                    * by main axis alignment space evenly
-                    * */
-
-                    Column(
-                      children: [
-                        Text(
-                          zOnBoardingTitle1,
-                          style: Theme.of(context).textTheme.headlineLarge,
-                        ),
-                        Text(
-                          zOnBoardingSubTitle1,
-                          style: Theme.of(context).textTheme.bodyLarge,
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ),
-                    Text(zOnBoardingCournter1),
-                    SizedBox(
-                      height: 50,
-                    )
-                  ],
-                ),
+              OnBoardingPageWidget(
+                model: OnBoardingModel(
+                  image: Assets.onboardingOnboarding1,
+              title: zOnBoardingTitle1,
+              subTitle: zOnBoardingSubTitle1,
+              counterText: zOnBoardingCournter1,
+              bgColor: zOnBoardingPage1Color,
+              height: size.height
               ),
-              Container(
-                color: zOnBoardingPage2Color,
               ),
-              Container(
-                color: zOnBoardingPage3Color,
-              ),
+
             ],
           ),
           /* Positioned(
@@ -76,6 +39,62 @@ class OnBoardingScreen extends StatelessWidget {
         ),
 
         SizedBox(height: 30,),*/
+        ],
+      ),
+    );
+  }
+}
+
+class OnBoardingPageWidget extends StatelessWidget {
+  const OnBoardingPageWidget({
+    super.key,
+    required this.model,
+  });
+
+  final OnBoardingModel model;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(zDefaultSizes),
+      color: model.bgColor,
+      child: Column(
+        /*
+      * main axis alignment in column means vertical alignment
+      * by default main axis size is max
+      * and cross axis alignment set to center
+      * which is horizontal alignment
+      * */
+
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset(
+            model.image,
+            height: model.height * 0.5,
+          ), //50% of devices height
+
+          /*
+          * column bellow will not be effected
+          * by main axis alignment space evenly
+          * */
+
+          Column(
+            children: [
+              Text(
+                model.title,
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+              Text(
+                model.subTitle,
+                style: Theme.of(context).textTheme.bodyLarge,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+          Text(model.counterText),
+          SizedBox(
+            height: 50,
+          )
         ],
       ),
     );
